5afe10971c4b58f9564a1dabe772dd4af95e636a
suncream100 <murmuring91@gmail.com>
2025-01-21 23:22:03 +0900

    section08 진행중 - 투두리스트 렌더링

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,15 +1,15 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-const List = () => {
+const List = ({ todos }) => {
   return (
     <div className="List">
       <h3>Todo List</h3>
       <input type="text" placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo) => {
+          return <TodoItem {...todo} />;
+        })}
       </div>
     </div>
   );
